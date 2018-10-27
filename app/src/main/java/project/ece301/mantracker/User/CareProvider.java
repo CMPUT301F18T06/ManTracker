452e03614b4e1f723e6cefa75355066c02438794
Tim Tran <ttran1@ualberta.ca>
2018-10-27 10:28:31 -0600

    Add class skeletion for patient and careprovider

--- app/src/main/java/project/ece301/mantracker/User/CareProvider.java
+++ app/src/main/java/project/ece301/mantracker/User/CareProvider.java
@@ -1,5 +1,22 @@
 package project.ece301.mantracker.User;
 
+import java.util.ArrayList;
+
 //
 public class CareProvider {
+    private ArrayList<Patient> patients;
+
+    public CareProvider() {
+
+    }
+
+    public Patient getPatient() {
+        return null;
+    }
+
+    public void addPatient() {
+
+    }
+
+    public void deletePatient() {}
 }
--- app/src/main/java/project/ece301/mantracker/User/Patient.java
+++ app/src/main/java/project/ece301/mantracker/User/Patient.java
@@ -1,5 +1,37 @@
 package project.ece301.mantracker.User;
 
+import java.util.ArrayList;
+
+import project.ece301.mantracker.MedicalProblem.BodyLocation;
+import project.ece301.mantracker.MedicalProblem.MedicalProblem;
+import project.ece301.mantracker.MedicalProblem.Record;
+
 //
 public class Patient {
+    private ArrayList<MedicalProblem> problemList;
+    private ArrayList<BodyLocation> bodyLocations;
+    private ArrayList<GeoLocation> geoLocations;
+    private ArrayList<Record> records;
+
+    public Patient() {
+
+    }
+
+    public MedicalProblem getProblem() { return null; }
+
+    public void addProblem(MedicalProblem problem) {}
+
+    public void deleteProblem(MedicalProblem problem) {}
+
+    public BodyLocation getBodyLocation(BodyLocation bodyLocation) { return null; }
+
+    public void addBodyLocation(BodyLocation bodyLocation) {}
+
+    public void deleteBodyLocation(BodyLocation bodyLocation) {}
+
+    public ArrayList<Record> getAllRecords() { return null; }
+
+    public void addRecord(Record record) {}
+
+    public void removeRecord(Integer rID) {}
 }
