a781b78c5b8a8119c377a80a6537c79c527d2d8a
John <bitancor@ualberta.ca>
2018-10-28 12:35:33 -0600

    Added unit tests

--- app/src/test/java/project/ece301/mantracker/BodyLocationTest.java
+++ app/src/test/java/project/ece301/mantracker/BodyLocationTest.java
@@ -2,11 +2,46 @@ package project.ece301.mantracker;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import project.ece301.mantracker.MedicalProblem.BodyLocation;
+
+import static org.junit.Assert.*;
 
 public class BodyLocationTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void getBodyPart() {
+        String expected = "Penis";
+        BodyLocation bodyLocation = new BodyLocation();
+        bodyLocation.setBodyPart("Penis");
+        assertEquals(expected, bodyLocation.getBodyPart());
+    }
+
+    @Test
+    public void setBodyPart() {
+        String expected = "Balls";
+        BodyLocation bodyLocation = new BodyLocation();
+        bodyLocation.setBodyPart("Balls");
+        assertEquals(expected, bodyLocation.getBodyPart());
+    }
+
+    @Test
+    public void setPhoto() {
+        String expected = "Nudes.jpeg";
+        BodyLocation bodyLocation = new BodyLocation();
+        bodyLocation.setPhoto("Nudes.jpeg");
+        assertEquals(expected, bodyLocation.getPhoto());
+    }
+
+    @Test
+    public void getPhoto() {
+        String expected = "Boobs.jpeg";
+        BodyLocation bodyLocation = new BodyLocation();
+        bodyLocation.setPhoto("Boobs.jpeg");
+        assertEquals(expected, bodyLocation.getPhoto());
+    }
+
+    @Test
+    public void showPhoto() {
+
     }
 }
