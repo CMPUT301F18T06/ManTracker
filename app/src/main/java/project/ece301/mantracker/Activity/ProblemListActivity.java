b19207e742cbd49561d328844c7ac06dc3a7f994
John <bitancor@ualberta.ca>
2018-11-08 20:21:13 -0700

    Added problem list activity file

---
+++ app/src/main/java/project/ece301/mantracker/Activity/ProblemListActivity.java
@@ -0,0 +1,13 @@
+package project.ece301.mantracker.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProblemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem_list);
+    }
+}
