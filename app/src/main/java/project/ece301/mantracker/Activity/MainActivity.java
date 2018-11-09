c3dfc751ed30e898a5c0a5842cab1c55f2c4162f
John <bitancor@ualberta.ca>
2018-11-08 20:35:26 -0700

    Pre-made UI

--- app/src/main/java/project/ece301/mantracker/Activity/MainActivity.java
+++ app/src/main/java/project/ece301/mantracker/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package project.ece301.mantracker.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import project.ece301.mantracker.R;
 
@@ -18,40 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                Intent problem_list_switch = new Intent(getApplicationContext(), ProblemListActivity.class);
+                startActivity(problem_list_switch);
+
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
 }
--- app/src/main/java/project/ece301/mantracker/Activity/ProblemListActivity.java
+++ app/src/main/java/project/ece301/mantracker/Activity/ProblemListActivity.java
@@ -3,6 +3,8 @@ package project.ece301.mantracker.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import project.ece301.mantracker.R;
+
 public class ProblemListActivity extends AppCompatActivity {
 
     @Override
