56db5cfa0844b8d1c2a4f0fdeb960939a54b0f33
LionKingSimba <ttran1@ualberta.ca>
2018-11-13 13:53:08 -0700

    Add EditProfilePresenter

--- app/src/main/java/project/ece301/mantracker/EditProfile/EditProfilePresenter.java
+++ app/src/main/java/project/ece301/mantracker/EditProfile/EditProfilePresenter.java
@@ -1,5 +1,34 @@
 package project.ece301.mantracker.EditProfile;
 
-public class EditProfilePresenter {
+import android.support.annotation.NonNull;
 
+import static android.support.v4.util.Preconditions.checkNotNull;
+
+public class EditProfilePresenter implements EditProfileContract.Presenter {
+
+    private final EditProfileContract.View mProfileView;
+
+    public EditProfilePresenter(@NonNull EditProfileContract.View profileView) {
+        mProfileView = profileView; // TODO: checkNotNull
+    }
+
+    @Override
+    public void loadUser() {
+
+    }
+
+    @Override
+    public void getUsername() {
+
+    }
+
+    @Override
+    public void getEmail() {
+
+    }
+
+    @Override
+    public void getPhone() {
+
+    }
 }
