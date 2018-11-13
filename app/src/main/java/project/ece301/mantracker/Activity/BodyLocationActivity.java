6700009a7fc3199a99a7e271194fb14572e504c5
John <bitancor@ualberta.ca>
2018-11-13 16:54:45 -0700

    Added zoom functionality

--- app/src/main/java/project/ece301/mantracker/Activity/BodyLocationActivity.java
+++ app/src/main/java/project/ece301/mantracker/Activity/BodyLocationActivity.java
@@ -1,9 +1,17 @@
 package project.ece301.mantracker.Activity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.github.chrisbanes.photoview.PhotoView;
 
 import project.ece301.mantracker.R;
 
@@ -14,4 +22,18 @@ public class BodyLocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body_location);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        PhotoView photoView = (PhotoView) findViewById(R.id.iSkeleton);
+        photoView.setImageResource(R.drawable.skeleton);
+
+//        ImageView imageView = findViewById(R.id.imageView);
+        final TextView textView = findViewById(R.id.text_coord);
+
+
+    }
+
 }
