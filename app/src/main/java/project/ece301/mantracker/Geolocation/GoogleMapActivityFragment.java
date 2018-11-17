503eabde15adc1dd515936b75a816b05412500f8
LionKingSimba <ttran1@ualberta.ca>
2018-11-17 12:04:53 -0700

    experiments with geolocation

---
+++ app/src/main/java/project/ece301/mantracker/Geolocation/GoogleMapActivityFragment.java
@@ -0,0 +1,24 @@
+package project.ece301.mantracker.Geolocation;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import project.ece301.mantracker.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GoogleMapActivityFragment extends Fragment {
+
+    public GoogleMapActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_google_map, container, false);
+    }
+}
