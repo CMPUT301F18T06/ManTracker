e635faa51186f63140768a2dfa3ef1a32ae745a8
LionKingSimba <ttran1@ualberta.ca>
2018-12-01 11:28:56 -0700

    Record and UploadPhoto javadocs

--- app/src/main/java/project/ece301/mantracker/MedicalProblem/Record.java
+++ app/src/main/java/project/ece301/mantracker/MedicalProblem/Record.java
@@ -1,3 +1,13 @@
+/**
+ * Class Name: Record
+ *
+ * Version: Version 1.0
+ *
+ * Date: November 31, 2018
+ *
+ * Copyright (c) Team 06, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ */
+
 package project.ece301.mantracker.MedicalProblem;
 
 import java.io.Serializable;
@@ -8,6 +18,11 @@ import java.util.UUID;
 
 import project.ece301.mantracker.details;
 
+/**
+ * Represents a Record
+ * Records have a date, ID, description, Title, problem ID, Patient, photos
+ * and BodyLocations
+ */
 public class Record implements details, Serializable { //implementing serializable allows us to pass an instance of record across activities
     private String date;
 
@@ -25,6 +40,9 @@ public class Record implements details, Serializable { //implementing serializab
 
     private ArrayList<String> photos;
 
+    /**
+     * Constructs a Record object
+     */
     public Record(){
 
          bodyLocations = new ArrayList<BodyLocation>();
@@ -32,75 +50,128 @@ public class Record implements details, Serializable { //implementing serializab
         ID = UUID.randomUUID().toString();
     }
 
+    /**
+     * Sets the associated patient of this record
+     * @param username the username of the patient to associate with this record
+     */
     public void setAssociatedPatient(String username) {
         associatedPatient = username;
     }
 
+    /**
+     * Gets the patient associated with this record
+     * @return the patient assosciated with this record
+     */
     public String getAssociatedPatient() {
         return associatedPatient;
     }
 
+    /**
+     * Gets the ID of the record
+     * @return the ID of this record
+     */
     public String getID() {
         return ID;
     }
 
-//
+    /**
+     * Get the ID of the problem associated with this record
+     * @return the ID of the problem associated with this record
+     */
     public String getProblemID() {
         return this.associatedProblemID;
     }
 
+    /**
+     * Set the ID of the problem associated with this record
+     * @param problemID the ID of the problem associated with this record
+     */
     public void setProblemID(String problemID) {
         this.associatedProblemID = problemID;
     }
 
+    /**
+     * Get the Date of this Record
+     * @return the date of this record
+     */
     public String getDate() {
         return date;
     }
-//
+
+    /**
+     * Set the date of this Record
+     * @param newDate the date to give to this record
+     */
     public void setDate(String newDate) {
         date = newDate;
     }
 
+    /**
+     * Get the title of this record
+     * @return the title of this record
+     */
     public String getTitle() {
         return Title;
     }
-//
+
+    /**
+     * Set the title of this record
+     * @param title the title to give to this record
+     */
     public void setTitle(String title) {
         Title = title;
 
     }
 
+    /**
+     * Get the description of this record
+     * @return the description of this record
+     */
     public String getDescription() {
         return Description;
     }
-//
+
+    /**
+     * Set the description of this record
+     * @param description the description to give to this record
+     */
     public void setDescription(String description) {
         Description = description;
     }
 //
 
     //    Input a BodyLocationPoint class
+
     public void deleteBodyLocationPoint(Object bdlocationpoint){
 
     }
 
+    /**
+     * Gets a list of BodyLocations associated with this record
+     * @return
+     */
     public ArrayList<BodyLocation> getBodyLocationList(){
         return bodyLocations;
     }
 
 
+    /**
+     * Adds a BodyLocation to this record
+     * @param bdlocation the bodylocation to add to this record
+     */
     // Input a bodylocation class
     public void addBodyLocation(BodyLocation bdlocation){
         bodyLocations.add(bdlocation);
     }
 
-
-
     public Object getBodyLocation(){
         return null;
     }
 
-
+    /**
+     * Adds a photo to this record
+     * @param photo
+     */
     public void addPhoto(String photo){
         photos.add(photo);
     }
@@ -114,10 +185,15 @@ public class Record implements details, Serializable { //implementing serializab
 
     }
 
+    /**
+     * Sets a GeoLocation to be associated with this record
+     * @param geolocation the GeoLocation to associate with this record
+     */
     //    Input a geolocation class
     public void setGeoLocation(Object geolocation){
 
     }
+
     public Object getGeoLocation(){
         return null;
     }
